59e26f5c305c7a976fc20b822445b3bb17dd0b7e
Gleb Getmanenko <mrp4p3r@gmail.com>
2019-10-20 21:52:23 +0300

    Added CliError repr

--- climaker/types/errors.py
+++ climaker/types/errors.py
@@ -1,7 +1,20 @@
+from typing import get_type_hints
+
+
 __all__ = [
     'CliError',
+    'ParsingError',
 ]
 
 
 class CliError:
+
+    def __repr__(self) -> str:
+        return '{}({})'.format(type(self).__name__, ', '.join((
+            f'{attr_name}={getattr(self, attr_name)}'
+            for attr_name in get_type_hints(type(self)).keys()
+        )))
+
+
+class ParsingError(CliError):
     pass
