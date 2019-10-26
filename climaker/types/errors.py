4e66fcf45b10f70fe82712f0ec04f0d2b31845f0
Gleb Getmanenko <mrp4p3r@gmail.com>
2019-10-26 18:03:56 +0300

    Fixup CliError repr

--- climaker/types/errors.py
+++ climaker/types/errors.py
@@ -11,7 +11,7 @@ class CliError:
 
     def __repr__(self) -> str:
         return '{}({})'.format(type(self).__name__, ', '.join((
-            f'{attr_name}={getattr(self, attr_name)}'
+            f'{attr_name}={getattr(self, attr_name)!r}'
             for attr_name in get_type_hints(type(self)).keys()
         )))
 
