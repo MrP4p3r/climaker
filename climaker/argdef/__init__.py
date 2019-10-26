944d11119d91f39b3c1822a2da3a7b486b9de03a
Gleb Getmanenko <mrp4p3r@gmail.com>
2019-10-27 01:57:03 +0300

    Fixup imports and FnParser

--- climaker/argdef/__init__.py
+++ climaker/argdef/__init__.py
@@ -1,3 +1,4 @@
 from .interfaces import *
 from .argdef import *
+from .value_parser import *
 from .value_reducer import *
--- climaker/argdef/value_parser.py
+++ climaker/argdef/value_parser.py
@@ -2,6 +2,11 @@ from typing import TypeVar, Protocol, Tuple, Optional
 from .interfaces import IValueParser
 
 
+__all__ = [
+    'FnParser',
+]
+
+
 V = TypeVar('V')
 
 
@@ -12,12 +17,12 @@ class _VarArgFn(Protocol[V]):
 
 class FnParser(IValueParser[V]):
 
-    def __init__(self, function: _VarArgFn[V], min_words: int = 0, max_words: Optional[int] = None):
+    def __init__(self, function: _VarArgFn[V], min_words: int = 1, max_words: Optional[int] = 1):
         assert 0 <= min_words
         assert max_words is None or min_words <= max_words
+        self._function = function
         self._min_words = min_words
         self._max_words = max_words
-        self._function = function
 
     def get_word_number_range(self) -> Tuple[int, Optional[int]]:
         return self._min_words, self._max_words
